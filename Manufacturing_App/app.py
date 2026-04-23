fc9b44478c91761c12a5a82d75d412f1a21e49f8
ryantheengineer <ryan.larson.wa@gmail.com>
2026-04-23 08:12:29 -0600

    Update app.py to use additional fields in config_vars.yaml for debug mode and port number

--- Manufacturing_App/app.py
+++ Manufacturing_App/app.py
@@ -902,5 +902,8 @@ def export_pdf(n_clicks, store):
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8050)
-    # app.run(debug=True, host="0.0.0.0", port=8050)
+    app.run(
+        debug=config_data.get('debug', False),
+        host="0.0.0.0",
+        port=config_data.get('port', 8050)
+    )
